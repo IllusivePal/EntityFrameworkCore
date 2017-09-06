3cb826a190fa57fceb25774b3e7e7fbd25351840
kyamc <kyamc@zylun.com>
2017-09-06 15:39:42 +0800

    EntityFramework Core for .net framework and .net core 2.0

---
+++ coresrc/SamuraiAppCore.Data/SamuraiContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiAppCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiAppCore.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Qoute> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                 "Server = ZYLUN-PC-0396\\SQL2014;Database = SamuraiDataCore;user id=sa;password=systemadmin "
+                );
+        }
+    }
+}
---
+++ coresrc/SamuraiAppCore.Domain/SamuraiAppCore.Domain/Samurai.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiAppCore.Domain
+{
+    public class Samurai
+    {
+        public Samurai()
+        {
+            Quotes = new List<Qoute>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Qoute> Quotes { get; set; }
+        public int BattleId { get; set; }
+    }
+}
